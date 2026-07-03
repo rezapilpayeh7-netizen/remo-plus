499a63c55acb4d667f3a2242faaafab2885dde08
REZA PIL PAYEH <rezapilpayeh7@gmail.com>
2026-07-03 09:31:47 +0330

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,10 +1,8 @@
-// js/supabase-config.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-// آدرس پروژه تو بر اساس آی‌دی تصویر
-const SUPABASE_URL = 'https://iksjymqykbptdtyjbqva.supabase.co'; 
-// کلید anon رو از پنل کپی کن و اینجا بذار
-const SUPABASE_KEY = 'sb_publishable_fp1VIVmax6ckJWllyLnB8w_qPn8ie3e'; 
+const SUPABASE_URL = 'https://iksjymqykbptdtyjbqva.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_fp1VIVmax6ckJWllyLnB8w_qPn8ie3e';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default supabase;
